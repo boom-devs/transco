c4105592972db64273d680f539c702d7727c1ff7
shiddik <shiddikurrahaman@gmail.com>
2023-05-20 13:17:46 +0600

    initail testimonial added

--- js/custom.js
+++ js/custom.js
@@ -1,256 +1,122 @@
 (function ($) {
-  "use strict";
-  $(document).ready(function () {
-    /*
-		   Preeloader
-		   ============================*/
-    $(window).on("load", function () {
-      $("#preloader").fadeOut();
-      $("#preloader-status").delay(200).fadeOut("slow");
-      $("body").delay(200).css({ "overflow-x": "hidden" });
-    });
-
-    /*
-		   Jquery Mobile Menu
-		   ============================*/
-    $("#main-menu").meanmenu({
-      meanScreenWidth: "991",
-      meanMenuContainer: ".mobile-nav-menu",
-    });
-
-    /*
-			Stikey Js
-			============================*/
-    var nav = $(".header-sec.sticky-header");
-    var scrolled = false;
-    $(window).scroll(function () {
-      if (100 < $(window).scrollTop() && !scrolled) {
-        nav
-          .addClass("sticky_menu animated fadeIn")
-          .animate({ "margin-top": "0px" });
-        scrolled = true;
-      }
-      if (100 > $(window).scrollTop() && scrolled) {
-        nav.removeClass("sticky_menu animated fadeIn").css("margin-top", "0px");
-        scrolled = false;
-      }
-    });
-
-    $(".custom-slider-wrapper").slick({
-      slidesToShow: 3,
-      infinite: false,
-      autoplay: true,
-      draggable: true,
-      arrows: false,
-      slidesToScroll: 1,
-      loop: true,
-      dots: true,
-      speed: 300,
-      prevArrow:
-        "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-      responsive: [
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            autoplay: true,
-            infinite: false,
-          },
-        },
-      ],
-    });
-    // portfolio slider home-2
-    $(".portfolio-slider").slick({
-      slidesToShow: 4,
-      infinite: false,
-      autoplay: false,
-      draggable: true,
-      arrows: true,
-      slidesToScroll: 1,
-      loop: true,
-      dots: true,
-      speed: 300,
-      prevArrow:
-        "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-      responsive: [
-        {
-          breakpoint: 991,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 767,
-          settings: {
-            slidesToShow: 1,
-            autoplay: true,
-            infinite: false,
-          },
-        },
-      ],
-    });
-    /*
-       Stikey Js
+    "use strict";
+    $(document).ready(function () {
+        /*
+       Preeloader
        ============================*/
-    $(".post-gallery").slick({
-      slidesToShow: 1,
-      infinite: false,
-      autoplay: false,
-      draggable: true,
-      arrows: true,
-      slidesToScroll: 1,
-      loop: true,
-      dots: false,
-      speed: 300,
-      prevArrow:
-        "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-    });
-    $(".slider-wrapper").slick({
-      slidesToShow: 1,
-      infinite: true,
-      autoplay: true,
-      draggable: true,
-      arrows: false,
-      slidesToScroll: 1,
-      loop: true,
-      dots: false,
-      speed: 1500,
-      prevArrow:
-        "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            autoplay: true,
-          },
-        },
-      ],
-    });
+        $(window).on("load", function () {
+            $("#preloader").fadeOut();
+            $("#preloader-status").delay(200).fadeOut("slow");
+            $("body").delay(200).css({"overflow-x": "hidden"});
+        });
 
-    $(".testimonial-slider").slick({
-      slidesToShow: 1,
-      infinite: false,
-      autoplay: false,
-      draggable: true,
-      arrows: true,
-      slidesToScroll: 1,
-      loop: true,
-      dots: false,
-      speed: 300,
-      prevArrow:
-        "<button type='button' class='testimonial-slider-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='testimonial-slider-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            autoplay: true,
-          },
-        },
-      ],
-    });
+        /*
+       Jquery Mobile Menu
+       ============================*/
+        $("#main-menu").meanmenu({
+            meanScreenWidth: "991",
+            meanMenuContainer: ".mobile-nav-menu",
+        });
 
-    // client-widget slider
-    $(".client-slider-wrapper").slick({
-      slidesToShow: 5,
-      infinite: true,
-      autoplay: true,
-      draggable: true,
-      arrows: false,
-      slidesToScroll: 1,
-      loop: true,
-      dots: false,
-      speed: 300,
-      prevArrow:
-        "<button type='button' class='client-slider-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='client-slider-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-      responsive: [
-        {
-          breakpoint: 1200,
-          settings: {
-            slidesToShow: 4,
-          },
-        },
-        {
-          breakpoint: 998,
-          settings: {
-            slidesToShow: 3,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 425,
-          settings: {
+        /*
+        Stikey Js
+        ============================*/
+        var nav = $(".header-sec.sticky-header");
+        var scrolled = false;
+        $(window).scroll(function () {
+            if (100 < $(window).scrollTop() && !scrolled) {
+                nav
+                    .addClass("sticky_menu animated fadeIn")
+                    .animate({"margin-top": "0px"});
+                scrolled = true;
+            }
+            if (100 > $(window).scrollTop() && scrolled) {
+                nav.removeClass("sticky_menu animated fadeIn").css("margin-top", "0px");
+                scrolled = false;
+            }
+        });
+
+
+        /*
+       Slider
+       ============================*/
+        $(".slider-wrapper").slick({
             slidesToShow: 1,
-          },
-        },
-      ],
-    });
+            infinite: true,
+            autoplay: true,
+            draggable: true,
+            arrows: false,
+            slidesToScroll: 1,
+            loop: true,
+            dots: false,
+            speed: 1500,
+            prevArrow:
+                "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
+            nextArrow:
+                "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        autoplay: true,
+                    },
+                },
+            ],
+        });
 
-    /*
-		   scrollUp
-		   ============================*/
-    $.scrollUp({
-      scrollText: '<i class="flaticon-scroll"></i>',
-      easingType: "linear",
-      scrollSpeed: 900,
-      animation: "fade",
-    });
-    /*
-	ounter Js
-	============================*/
-    $(".counter").counterUp({
-      delay: 10,
-      time: 1000,
-    });
+        /*
+       Testimonial Slider
+       ============================*/
+        $(".testimonial-slider-wrapper").slick({
+            slidesToShow: 2,
+            infinite: true,
+            autoplay: true,
+            draggable: true,
+            arrows: false,
+            slidesToScroll: 1,
+            loop: true,
+            dots: false,
+            speed: 1500,
+            prevArrow:
+                "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
+            nextArrow:
+                "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
+            responsive: [
+                {
+                    breakpoint: 767,
+                    settings: {
+                        autoplay: true,
+                    },
+                },
+            ],
+        });
+        /*
+       scrollUp
+       ============================*/
+        $.scrollUp({
+            scrollText: '<i class="flaticon-scroll"></i>',
+            easingType: "linear",
+            scrollSpeed: 900,
+            animation: "fade",
+        });
+        /*
+        ounter Js
+        ============================*/
+        $(".counter").counterUp({
+            delay: 10,
+            time: 1000,
+        });
 
-    /*
-		   Magnific Popup
-		   ============================*/
-    $(".video-play").magnificPopup({
-      disableOn: 700,
-      type: "iframe",
-      mainClass: "mfp-fade",
-      removalDelay: 160,
-      preloader: false,
-      fixedContentPos: false,
+        /*
+       Magnific Popup
+       ============================*/
+        $(".video-play").magnificPopup({
+            disableOn: 700,
+            type: "iframe",
+            mainClass: "mfp-fade",
+            removalDelay: 160,
+            preloader: false,
+            fixedContentPos: false,
+        });
     });
-
-    $(".hero-slider").slick({
-      slidesToShow: 1,
-      infinite: false,
-      autoplay: false,
-      draggable: true,
-      arrows: true,
-      slidesToScroll: 1,
-      loop: true,
-      dots: false,
-      speed: 300,
-      prevArrow:
-        "<button type='button' class='post-gallery-btn prev-btn'><i class='fa fa-arrow-left'></i></button>",
-      nextArrow:
-        "<button type='button' class='post-gallery-btn next-btn'><i class='fa fa-arrow-right'></i></button>",
-    });
-  });
 })(jQuery);
