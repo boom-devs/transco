6da5f74596187d8c51117d54946989584e1e8bec
shiddik <shiddikurrahaman@gmail.com>
2023-06-24 11:09:34 +0600

    fix sticky header height in custom js

--- js/custom.js
+++ js/custom.js
@@ -36,13 +36,13 @@
         const nav = $(".header-menu-area.sticky-header");
         let scrolled = false;
         $(window).scroll(function () {
-            if (100 < $(window).scrollTop() && !scrolled) {
+            if (80 < $(window).scrollTop() && !scrolled) {
                 nav
                     .addClass("sticky_menu animated fadeIn")
                     .animate({"margin-top": "0px"});
                 scrolled = true;
             }
-            if (100 > $(window).scrollTop() && scrolled) {
+            if (80 > $(window).scrollTop() && scrolled) {
                 nav.removeClass("sticky_menu animated fadeIn").css("margin-top", "0px");
                 scrolled = false;
             }
